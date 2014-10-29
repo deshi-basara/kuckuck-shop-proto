f42d99cfb9ed36dae5b89818a597c568b0275936
simon <simon@i-was-perfect.net>
2014-10-29 15:36:53 +0100

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,56 @@
+'use strict';
+
+// angular.module is a global place for creating, registering and retrieving Angular modules.
+// 'app' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'.
+angular
+  .module('app', [
+  'ui.router',
+  'LocalStorageModule',
+  'ngAnimate',
+  'ngTable',
+  'oitozero.ngSweetAlert'
+])
+
+.constant('config', {
+  'name': 'development',
+  'apiUrl': 'http://localhost:1337'
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // This starter uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  $stateProvider
+
+    .state('landing', {
+      url: '/',
+      templateUrl: 'scripts/routes/landing/landing.index.tpl.html',
+      controller: 'LandingCtrl',
+      controllerAs: 'ctrl'
+    })
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/');
+
+})
+
+.run(function($state, AuthService) {
+
+  // check if the user has a active session
+  AuthService.hasSession()
+    .then(function(success) {
+      console.log(success);
+    }, function(error) {
+      alert(error);
+    });
+
+
+  var hasAuth = false;
+
+  if(hasAuth) {
+    console.log('rediec');
+    $state.go('login');
+  }
+})
---
+++ app/scripts/routes/landing/landing.controller.js
@@ -0,0 +1,24 @@
+(function () {
+
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('LandingCtrl', LandingCtrl);
+
+    LandingCtrl.$inject = [];
+
+    /**
+     * Handles the dash-board view and all interactions
+     */
+    function LandingCtrl() {
+        var ctrl = this;
+
+        //////////////////////
+
+        angular.extend(ctrl, {
+
+        });
+    }
+
+})();
