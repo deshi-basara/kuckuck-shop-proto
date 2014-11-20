fb9d4c3d896a1603e34b939126d3db05bc92635b
simon <simon@i-was-perfect.net>
2014-11-20 18:51:22 +0100

    Highlight the active menu item

--- app/scripts/routes/main/main.controller.js
+++ app/scripts/routes/main/main.controller.js
@@ -6,13 +6,23 @@
         .module('app')
         .controller('MainCtrl', MainCtrl);
 
-    MainCtrl.$inject = ['$scope'];
+    MainCtrl.$inject = ['$scope', '$location'];
 
     /**
      * Handles the landing view and all interactions
      */
-    function MainCtrl($scope) {
+    function MainCtrl($scope, $location) {
         var main = this;
+
+        /**
+         * Checks if the navigation link is active
+         * @param  {string}  url [Url of the navigation link]
+         * @return {Boolean}     [description]
+         */
+        function isActive(url) {
+            console.log(url + ' === ' + $location.url());
+            return url === $location.url();
+        }
 
         /**
          * Toggles the shopping car open state.
@@ -25,6 +35,7 @@
          * Listens for requests to see the loader
          */
         $scope.$on('loader.show', function() {
+            console.log('show');
             $scope.$apply(function() {
                 main.isLoading = true;
             });
@@ -34,6 +45,7 @@
          * Listens for requests to hide the loader
          */
         $scope.$on('loader.hide', function() {
+            console.log('hide');
             $scope.$apply(function() {
                 main.isLoading = false;
             });
@@ -45,6 +57,7 @@
             bagOpen: false,
             isLoading: true,
 
+            isActive: isActive,
             toggleBag: toggleBag
         });
     }
