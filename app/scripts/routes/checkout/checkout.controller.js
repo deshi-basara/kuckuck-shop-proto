f28b0cf26930bc79124c5375c72bdad412bd5e30
Deshi Basara <simon@i-was-perfect.net>
2014-12-03 19:16:18 +0100

    Checkout with pagepiling implemented

--- app/scripts/routes/checkout/checkout.controller.js
+++ app/scripts/routes/checkout/checkout.controller.js
@@ -6,25 +6,72 @@
         .module('app')
         .controller('CheckoutCtrl', CheckoutCtrl);
 
-    CheckoutCtrl.$inject = ['$rootScope', '$timeout'];
+    CheckoutCtrl.$inject = ['$rootScope', '$timeout', 'SweetAlert'];
 
     /**
      * Handles the landing view and all interactions
      */
-    function CheckoutCtrl($rootScope, $timeout) {
+    function CheckoutCtrl($rootScope, $timeout, SweetAlert) {
         var ctrl = this;
 
         /**
          * Initiate pagepilling when the dom is ready
          */
         angular.element(document).ready(function () {
-            $timeout(function() {
-                $rootScope.$broadcast('loader.hide');
-            }, 500);
+            setTimeout(function() {
+
+                $('#checkout').pagepiling({
+                    menu: null,
+                    direction: 'vertical',
+                    verticalCentered: true,
+                    sectionsColor: ['#7BAABE', '#7BAABE', '#7BAABE'],
+                    anchors: [],
+                    scrollingSpeed: 700,
+                    easing: 'swing',
+                    loopBottom: false,
+                    loopTop: false,
+                    css3: true,
+                    navigation: false,
+                    normalScrollElements: null,
+                    normalScrollElementTouchThreshold: 5,
+                    touchSensitivity: 5,
+                    keyboardScrolling: true,
+                    sectionSelector: '.section',
+                    animateAnchor: false,
+
+                    //events
+                    onLeave: function(index, nextIndex, direction) {},
+                    afterLoad: function(anchorLink, index){
+                    },
+                    afterRender: function(){
+
+                        // hide the loader
+                        $timeout(function() {
+                            $rootScope.$broadcast('loader.hide');
+                        }, 500);
+                    },
+                });
+
+            }, 10);
         });
 
+        /**
+         * Pagepiling scroll down
+         */
+        function scrollDown() {
+            $.fn.pagepiling.moveSectionDown();
+        }
 
+        function scrollUp() {
+            $.fn.pagepiling.moveSectionUp();
+        }
 
+        /**
+         * Fake checkout submit
+         */
+        function submitCheckout() {
+            SweetAlert.swal("Oh noooo", "Diese Funktionalität hat es leider nicht in den Prototypen geschafft :'-(");
+        }
 
         //////////////////////
 
@@ -32,7 +79,7 @@
             check: {
                 account: false,
                 discount: false,
-                shipping: true,
+                shipping: false,
                 pay: {
                     paypal: false,
                     credit: false,
@@ -40,7 +87,11 @@
                 }
             },
             form: {
-            }
+            },
+
+            scrollDown: scrollDown,
+            scrollUp: scrollUp,
+            submitCheckout: submitCheckout
         });
 
         //////////////////////
